b6ff4eabed0c449c043db1668c9526404e3a3ebb
Ryan Jackson <ryanwjackson@gmail.com>
2021-05-27 16:55:54 +0200

    Rubocop

--- spec/support/quickbooks_online_helpers.rb
+++ spec/support/quickbooks_online_helpers.rb
@@ -225,53 +225,53 @@ module QuickBooksOnlineHelpers # rubocop:disable Metrics/ModuleLength
     stub_request(:post, 'https://sandbox-quickbooks.api.intuit.com/v3/company/realm_id/purchase')
       .with(
         body: {
-          "Id": nil,
-          "CurrencyRef": {
-            "value": 'USD'
+          Id: nil,
+          CurrencyRef: {
+            value: 'USD'
           },
-          "PaymentType": 'Cash',
-          "TxnDate": '2019-09-01',
-          "PrivateNote": 'Memo',
-          "ExchangeRate": 1.0,
-          "EntityRef": {
-            "value": '123',
-            "type": 'Customer'
+          PaymentType: 'Cash',
+          TxnDate: '2019-09-01',
+          PrivateNote: 'Memo',
+          ExchangeRate: 1.0,
+          EntityRef: {
+            value: '123',
+            type: 'Customer'
           },
-          "DocNumber": 'Ref123',
-          "AccountRef": {
-            "value": '123'
+          DocNumber: 'Ref123',
+          AccountRef: {
+            value: '123'
           },
-          "DepartmentRef": {
-            "value": '123'
+          DepartmentRef: {
+            value: '123'
           },
-          "Line": [
+          Line: [
             {
-              "Id": nil,
-              "DetailType": 'AccountBasedExpenseLineDetail',
-              "AccountBasedExpenseLineDetail": {
-                "AccountRef": {
-                  "value": '123'
+              Id: nil,
+              DetailType: 'AccountBasedExpenseLineDetail',
+              AccountBasedExpenseLineDetail: {
+                AccountRef: {
+                  value: '123'
                 },
-                "ClassRef": {
-                  "value": '123'
+                ClassRef: {
+                  value: '123'
                 }
               },
-              "Amount": 123.45,
-              "Description": 'Sample Transaction'
+              Amount: 123.45,
+              Description: 'Sample Transaction'
             },
             {
-              "Id": nil,
-              "DetailType": 'AccountBasedExpenseLineDetail',
-              "AccountBasedExpenseLineDetail": {
-                "AccountRef": {
-                  "value": '123'
+              Id: nil,
+              DetailType: 'AccountBasedExpenseLineDetail',
+              AccountBasedExpenseLineDetail: {
+                AccountRef: {
+                  value: '123'
                 },
-                "ClassRef": {
-                  "value": '123'
+                ClassRef: {
+                  value: '123'
                 }
               },
-              "Amount": 123.45,
-              "Description": 'Sample Transaction'
+              Amount: 123.45,
+              Description: 'Sample Transaction'
             }
           ]
         },
@@ -286,97 +286,97 @@ module QuickBooksOnlineHelpers # rubocop:disable Metrics/ModuleLength
       .to_return(
         status: 200,
         body: {
-          "Purchase": {
-            "AccountRef": {
-              "value": '123',
-              "name": 'Sample Account'
+          Purchase: {
+            AccountRef: {
+              value: '123',
+              name: 'Sample Account'
             },
-            "PaymentType": 'Cash',
-            "EntityRef": {
-              "value": '123',
-              "name": 'Sample Vendor',
-              "type": 'Vendor'
+            PaymentType: 'Cash',
+            EntityRef: {
+              value: '123',
+              name: 'Sample Vendor',
+              type: 'Vendor'
             },
-            "TotalAmt": 24_690.0,
-            "PurchaseEx": {
-              "any": [
+            TotalAmt: 24_690.0,
+            PurchaseEx: {
+              any: [
                 {
-                  "name": '{http://schema.intuit.com/finance/v3}NameValue',
-                  "declaredType": 'com.intuit.schema.finance.v3.NameValue',
-                  "scope": 'javax.xml.bind.JAXBElement$GlobalScope',
-                  "value": {
-                    "Name": 'TxnType',
-                    "Value": '54'
+                  name: '{http://schema.intuit.com/finance/v3}NameValue',
+                  declaredType: 'com.intuit.schema.finance.v3.NameValue',
+                  scope: 'javax.xml.bind.JAXBElement$GlobalScope',
+                  value: {
+                    Name: 'TxnType',
+                    Value: '54'
                   },
-                  "nil": false,
-                  "globalScope": true,
-                  "typeSubstituted": false
+                  nil: false,
+                  globalScope: true,
+                  typeSubstituted: false
                 }
               ]
             },
-            "domain": 'QBO',
-            "sparse": false,
-            "Id": '123',
-            "SyncToken": '0',
-            "MetaData": {
-              "CreateTime": '2019-09-20T09:44:50-07:00',
-              "LastUpdatedTime": '2019-09-20T09:44:50-07:00'
+            domain: 'QBO',
+            sparse: false,
+            Id: '123',
+            SyncToken: '0',
+            MetaData: {
+              CreateTime: '2019-09-20T09:44:50-07:00',
+              LastUpdatedTime: '2019-09-20T09:44:50-07:00'
             },
-            "CustomField": [],
-            "DocNumber": 'Ref123',
-            "DepartmentRef": {
-              "value": '123',
-              "name": 'Sample Department'
+            CustomField: [],
+            DocNumber: 'Ref123',
+            DepartmentRef: {
+              value: '123',
+              name: 'Sample Department'
             },
-            "TxnDate": '2019-09-01',
-            "CurrencyRef": {
-              "value": 'USD'
+            TxnDate: '2019-09-01',
+            CurrencyRef: {
+              value: 'USD'
             },
-            "PrivateNote": 'Memo',
-            "Line": [
+            PrivateNote: 'Memo',
+            Line: [
               {
-                "Id": '1',
-                "Description": 'Sample Transaction',
-                "Amount": 123.45,
-                "DetailType": 'AccountBasedExpenseLineDetail',
-                "AccountBasedExpenseLineDetail": {
-                  "AccountRef": {
-                    "value": '123',
-                    "name": 'Sample Account'
+                Id: '1',
+                Description: 'Sample Transaction',
+                Amount: 123.45,
+                DetailType: 'AccountBasedExpenseLineDetail',
+                AccountBasedExpenseLineDetail: {
+                  AccountRef: {
+                    value: '123',
+                    name: 'Sample Account'
                   },
-                  "BillableStatus": 'NotBillable',
-                  "ClassRef": {
-                    "value": '123',
-                    "name": 'Sample Class'
+                  BillableStatus: 'NotBillable',
+                  ClassRef: {
+                    value: '123',
+                    name: 'Sample Class'
                   },
-                  "TaxCodeRef": {
-                    "value": 'NON'
+                  TaxCodeRef: {
+                    value: 'NON'
                   }
                 }
               },
               {
-                "Id": '2',
-                "Description": 'Sample Transaction',
-                "Amount": 123.45,
-                "DetailType": 'AccountBasedExpenseLineDetail',
-                "AccountBasedExpenseLineDetail": {
-                  "AccountRef": {
-                    "value": '123',
-                    "name": 'Sample Account'
+                Id: '2',
+                Description: 'Sample Transaction',
+                Amount: 123.45,
+                DetailType: 'AccountBasedExpenseLineDetail',
+                AccountBasedExpenseLineDetail: {
+                  AccountRef: {
+                    value: '123',
+                    name: 'Sample Account'
                   },
-                  "BillableStatus": 'NotBillable',
-                  "ClassRef": {
-                    "value": '123',
-                    "name": 'Sample Class'
+                  BillableStatus: 'NotBillable',
+                  ClassRef: {
+                    value: '123',
+                    name: 'Sample Class'
                   },
-                  "TaxCodeRef": {
-                    "value": 'NON'
+                  TaxCodeRef: {
+                    value: 'NON'
                   }
                 }
               }
             ]
           },
-          "time": '2019-09-20T09:44:50.133-07:00'
+          time: '2019-09-20T09:44:50.133-07:00'
         }.to_json,
         headers: {}
       )
--- spec/support/records/deposit.rb
+++ spec/support/records/deposit.rb
@@ -9,172 +9,172 @@ module Test
           ledger_class: name.demodulize,
           ledger_resource: 'Deposit',
           request_hash: {
-            "Id": nil,
-            "CurrencyRef": {
-              "value": 'USD'
+            Id: nil,
+            CurrencyRef: {
+              value: 'USD'
             },
-            "TxnDate": '2019-09-01',
-            "PrivateNote": 'Memo',
-            "ExchangeRate": 1.0,
-            "DepositToAccountRef": {
-              "value": '123'
+            TxnDate: '2019-09-01',
+            PrivateNote: 'Memo',
+            ExchangeRate: 1.0,
+            DepositToAccountRef: {
+              value: '123'
             },
-            "DepartmentRef": {
-              "value": '123'
+            DepartmentRef: {
+              value: '123'
             },
-            "Line": [
+            Line: [
               {
-                "Id": nil,
-                "DetailType": 'DepositLineDetail',
-                "DepositLineDetail": {
-                  "AccountRef": {
-                    "value": '123'
+                Id: nil,
+                DetailType: 'DepositLineDetail',
+                DepositLineDetail: {
+                  AccountRef: {
+                    value: '123'
                   },
-                  "ClassRef": {
-                    "value": '123'
+                  ClassRef: {
+                    value: '123'
                   },
-                  "Entity": {
-                    "value": '123',
-                    "name": 'Sample Vendor',
-                    "type": 'Vendor'
+                  Entity: {
+                    value: '123',
+                    name: 'Sample Vendor',
+                    type: 'Vendor'
                   }
                 },
-                "Amount": 123.45,
-                "Description": 'Sample Transaction 1'
+                Amount: 123.45,
+                Description: 'Sample Transaction 1'
               },
               {
-                "Id": nil,
-                "DetailType": 'DepositLineDetail',
-                "DepositLineDetail": {
-                  "AccountRef": {
-                    "value": '123'
+                Id: nil,
+                DetailType: 'DepositLineDetail',
+                DepositLineDetail: {
+                  AccountRef: {
+                    value: '123'
                   },
-                  "ClassRef": {
-                    "value": '123'
+                  ClassRef: {
+                    value: '123'
                   },
-                  "Entity": nil
+                  Entity: nil
                 },
-                "Amount": 123.45,
-                "Description": 'Sample Transaction 2'
+                Amount: 123.45,
+                Description: 'Sample Transaction 2'
               }
             ]
           },
           update_request_hash: {
-            "Id": '123',
-            "CurrencyRef": {
-              "value": 'USD'
+            Id: '123',
+            CurrencyRef: {
+              value: 'USD'
             },
-            "TxnDate": '2019-09-01',
-            "PrivateNote": 'Memo',
-            "ExchangeRate": 1.0,
-            "DepositToAccountRef": {
-              "value": '123',
-              "name": 'Cash on hand'
+            TxnDate: '2019-09-01',
+            PrivateNote: 'Memo',
+            ExchangeRate: 1.0,
+            DepositToAccountRef: {
+              value: '123',
+              name: 'Cash on hand'
             },
-            "DepartmentRef": {
-              "value": '123',
-              "name": 'Sample Department'
+            DepartmentRef: {
+              value: '123',
+              name: 'Sample Department'
             },
-            "Line": [
+            Line: [
               {
-                "Id": nil,
-                "DetailType": 'DepositLineDetail',
-                "DepositLineDetail": {
-                  "AccountRef": {
-                    "value": '123'
+                Id: nil,
+                DetailType: 'DepositLineDetail',
+                DepositLineDetail: {
+                  AccountRef: {
+                    value: '123'
                   },
-                  "ClassRef": {
-                    "value": '123'
+                  ClassRef: {
+                    value: '123'
                   },
-                  "Entity": {
-                    "value": '123',
-                    "name": 'Sample Vendor',
-                    "type": 'Vendor'
+                  Entity: {
+                    value: '123',
+                    name: 'Sample Vendor',
+                    type: 'Vendor'
                   }
                 },
-                "Amount": 123.45,
-                "Description": 'Sample Transaction 1'
+                Amount: 123.45,
+                Description: 'Sample Transaction 1'
               },
               {
-                "Id": nil,
-                "DetailType": 'DepositLineDetail',
-                "DepositLineDetail": {
-                  "AccountRef": {
-                    "value": '123'
+                Id: nil,
+                DetailType: 'DepositLineDetail',
+                DepositLineDetail: {
+                  AccountRef: {
+                    value: '123'
                   },
-                  "ClassRef": {
-                    "value": '123'
+                  ClassRef: {
+                    value: '123'
                   },
-                  "Entity": nil
+                  Entity: nil
                 },
-                "Amount": 123.45,
-                "Description": 'Sample Transaction 2'
+                Amount: 123.45,
+                Description: 'Sample Transaction 2'
               }
             ],
-            "TotalAmt": 246.9,
-            "domain": 'QBO',
-            "sparse": false,
-            "SyncToken": '0',
-            "MetaData": {
-              "CreateTime": '2019-10-11T11:54:50-07:00',
-              "LastUpdatedTime": '2019-10-11T11:54:50-07:00'
+            TotalAmt: 246.9,
+            domain: 'QBO',
+            sparse: false,
+            SyncToken: '0',
+            MetaData: {
+              CreateTime: '2019-10-11T11:54:50-07:00',
+              LastUpdatedTime: '2019-10-11T11:54:50-07:00'
             }
           },
           response_hash: {
-            "DepositToAccountRef": {
-              "value": '123',
-              "name": 'Cash on hand'
+            DepositToAccountRef: {
+              value: '123',
+              name: 'Cash on hand'
             },
-            "TotalAmt": 246.9,
-            "domain": 'QBO',
-            "sparse": false,
-            "Id": '123',
-            "SyncToken": '0',
-            "MetaData": {
-              "CreateTime": '2019-10-11T11:54:50-07:00',
-              "LastUpdatedTime": '2019-10-11T11:54:50-07:00'
+            TotalAmt: 246.9,
+            domain: 'QBO',
+            sparse: false,
+            Id: '123',
+            SyncToken: '0',
+            MetaData: {
+              CreateTime: '2019-10-11T11:54:50-07:00',
+              LastUpdatedTime: '2019-10-11T11:54:50-07:00'
             },
-            "TxnDate": '2019-09-01',
-            "DepartmentRef": {
-              "value": '123',
-              "name": 'Sample Department'
+            TxnDate: '2019-09-01',
+            DepartmentRef: {
+              value: '123',
+              name: 'Sample Department'
             },
-            "CurrencyRef": {
-              "value": 'USD'
+            CurrencyRef: {
+              value: 'USD'
             },
-            "PrivateNote": 'Memo',
-            "Line": [
+            PrivateNote: 'Memo',
+            Line: [
               {
-                "Id": '1',
-                "LineNum": 1,
-                "Description": 'Sample Transaction 1',
-                "Amount": 123.45,
-                "DetailType": 'DepositLineDetail',
-                "DepositLineDetail": {
-                  "AccountRef": {
-                    "value": '123',
-                    "name": 'Cash on hand'
+                Id: '1',
+                LineNum: 1,
+                Description: 'Sample Transaction 1',
+                Amount: 123.45,
+                DetailType: 'DepositLineDetail',
+                DepositLineDetail: {
+                  AccountRef: {
+                    value: '123',
+                    name: 'Cash on hand'
                   },
-                  "ClassRef": {
-                    "value": '123',
-                    "name": 'Sample Class'
+                  ClassRef: {
+                    value: '123',
+                    name: 'Sample Class'
                   }
                 }
               },
               {
-                "Id": '2',
-                "LineNum": 2,
-                "Description": 'Sample Transaction 2',
-                "Amount": 123.45,
-                "DetailType": 'DepositLineDetail',
-                "DepositLineDetail": {
-                  "AccountRef": {
-                    "value": '123',
-                    "name": 'Cash on hand'
+                Id: '2',
+                LineNum: 2,
+                Description: 'Sample Transaction 2',
+                Amount: 123.45,
+                DetailType: 'DepositLineDetail',
+                DepositLineDetail: {
+                  AccountRef: {
+                    value: '123',
+                    name: 'Cash on hand'
                   },
-                  "ClassRef": {
-                    "value": '123',
-                    "name": 'Sample Class'
+                  ClassRef: {
+                    value: '123',
+                    name: 'Sample Class'
                   }
                 }
               }
